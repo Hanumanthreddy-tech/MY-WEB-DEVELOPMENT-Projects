b018687ddfbe3a27c3b3b7cde996eecce20d5dcc
Hanumanth Reddy <Reddyhanumanth127@gmail.com>
2024-12-20 18:25:31 +0530

    message

---
+++ my portfolio website/profile.js
@@ -0,0 +1,6 @@
+var newone = new Typed(".auto-type", {
+    strings: ["Programmer. . . !", "Designer . . . !", "Web Developer . . . !", 'Full stack Developer. !'],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
