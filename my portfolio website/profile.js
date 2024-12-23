afb68ac45443b7390edc9a8fe07f7a39f2e5b36b
Hanumanth Reddy <Reddyhanumanth127@gmail.com>
2024-12-23 20:54:30 +0530

    Update profile.js

--- my portfolio website/profile.js
+++ my portfolio website/profile.js
@@ -1,6 +1,20 @@
 var newone = new Typed(".auto-type", {
-    strings: ["Programmer. . . !", "Designer . . . !", "Web Developer . . . !", 'Full stack Developer. !'],
+    strings: ["Programmer. . . !", "Designer . . . !", "Web Developer . . . !"],
     typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
+    backSpeed: 100,
+    loop: true,
+    startDelay: 4500,
+
+})
+
+var newone = new Typed(".myName", {
+    strings: ["Hanumanth Reddy"],
+    typeSpeed: 200,
+    showCursor: false
+})
+
+var newone = new Typed(".helloID", {
+    strings: ["Hello I'm"],
+    typeSpeed: 100,
+    showCursor: false
 })
